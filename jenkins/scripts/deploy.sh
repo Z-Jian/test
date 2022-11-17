c565c135890ae8cbd4b7a7a0e3cd7fedea318212
Z-Jian <2002689@sit.singaporetech.edu.sg>
2022-11-17 14:52:13 +0800

    test

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -5,5 +5,6 @@ docker run -d -p 80:80 --name my-apache-php-app -v /var/jenkins_home/workspace/t
 sleep 1
 set +x
 
+
 echo 'Now...'
 echo 'Visit http://localhost to see your PHP application in action.'
